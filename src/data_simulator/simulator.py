50e367a936f974f05fe9039ed72fc6a8b3d99929
K0405Y <kennymichael0405@gmail.com>
2024-10-31 12:28:03 +0100

    new data simulation with change to parquet

--- src/data_simulator/simulator.py
+++ src/data_simulator/simulator.py
@@ -227,11 +227,14 @@ if not os.path.exists(DIR_OUTPUT):
 
 start_date = datetime.datetime.strptime("2024-10-29", "%Y-%m-%d")
 
-for day in range(transactions_df.TX_TIME_DAYS.max()+1):
+for day in range(transactions_df.TX_TIME_DAYS.max() + 1):
     
-    transactions_day = transactions_df[transactions_df.TX_TIME_DAYS==day].sort_values('TX_TIME_SECONDS')
+    # Filter for transactions on the current day and sort by seconds
+    transactions_day = transactions_df[transactions_df.TX_TIME_DAYS == day].sort_values('TX_TIME_SECONDS')
     
+    # Format the filename based on the date
     date = start_date + datetime.timedelta(days=day)
-    filename_output = date.strftime("%Y-%m-%d")+'.pkl'
+    filename_output = date.strftime("%Y-%m-%d") + '.parquet'
     
-    transactions_day.to_pickle(DIR_OUTPUT+filename_output, protocol=4)
+    # Save as Parquet
+    transactions_day.to_parquet(os.path.join(DIR_OUTPUT, filename_output), index=False)
